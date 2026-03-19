5e99430a662ace9cc0f20d8ebce436165f36c120
xnelma <151833954+xnelma@users.noreply.github.com>
2026-03-19 23:41:23 +0100

    chore: fill database with sample data

--- qml/data.js
+++ qml/data.js
@@ -34,8 +34,52 @@ function openDatabase()
                       'primaryColor TEXT, secondaryColor TEXT, ' +
                       'tags TEXT)');
 
-        // TODO fill database with sample data
+        var rs = tx.executeSql('SELECT * FROM Collection');
+        if (rs.rows.length === 0) {
+            Data.fillDatabase(db);
+        }
     });
 
     return db;
+}
+
+function fillDatabase(db)
+{
+    var data = parseSampleData();
+
+    db.transaction(
+        function(tx) {
+            var res = tx.executeSql('SELECT * FROM Collection');
+            if (res.rows.length !== 0)
+                return;
+
+            data.forEach(function(item) {
+                tx.executeSql(
+                    'INSERT INTO Collection VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                    [item.title,
+                     item.origin, item.originColor,
+                     item.type,
+                     item.age, item.ageUnit,
+                     item.weightTotal, item.weightLeft,
+                     item.weightUnit,
+                     item.cost, item.secondaryCost,
+                     item.primaryColor, item.secondaryColor,
+                     tagStr(item.tags)]
+                );
+            });
+        }
+    );
+}
+
+function tagStr(tags)
+{
+    var tagStr = "";
+    tags.forEach(function (s) {
+        tagStr = tagStr + s + "|";
+    });
+
+    if (tagStr.length > 0 && tagStr[tagStr.length - 1] === "|")
+        tagStr = tagStr.substring(0, tagStr.length - 1);
+
+    return tagStr;
 }
